729ebaef994c12dd7ee153725cab2ce577f7ccd1
MaheshKotambe <145077930+MaheshKotambe@users.noreply.github.com>
2026-04-22 23:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function daysOfAYear(y){
+	return isLeap(y)? 366 : 365
+}
+
+function isLeap(y){
+	if(y%4==0){
+		if(y%100==0){
+			if(y%400==0){
+				return true
+			}else{
+				return false
+			}
+		}else{
+			return true
+		}
+	}else{
+		return false
+	}
+}
